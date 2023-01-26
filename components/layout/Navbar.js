e1d793b2c2130103855d85003253baa7c2bd3642
federerkristijan <federer.kristijan@gmail.com>
2023-01-26 21:29:39 +0100

    navbar done

--- components/layout/Navbar.js
+++ components/layout/Navbar.js
@@ -1,8 +1,32 @@
+import Link from "next/link"
 
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <Link href='/about'>About</Link>
+          </li>
+          <li>
+            <Link href='/contact'>Contact us</Link>
+          </li>
+          <li>
+            <Link href='/reviews'>Reviews</Link>
+          </li>
+          <li>
+            <Link href='/sessions'>Sessions</Link>
+          </li>
+          <li>
+            <Link href='/impressum'>Impressum</Link>
+          </li>
+          <li>
+            <Link href='/booking'>Booking</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
   )
 }
 
