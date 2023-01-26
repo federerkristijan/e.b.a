4e41833b0c653022fb45b4efb7c8793ed1810d7f
federerkristijan <federer.kristijan@gmail.com>
2023-01-26 21:37:20 +0100

    layout.js and layout.module.css done

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,6 +1,13 @@
+import Navbar from "./Navbar"
+
+import classes from "./Layout.module.css"
+
 const Layout = () => {
   return (
-    <div>Layout</div>
+    <div>
+      <Navbar />
+      <main className={classes.main}>{props.children}</main>
+    </div>
   )
 }
 
