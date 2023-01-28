1139fdebcea7bcd66b558205b37ba288be0809e3
federerkristijan <federer.kristijan@gmail.com>
2023-01-28 12:24:28 +0100

    sanity added

---
+++ eba/schemas/home.js
@@ -0,0 +1,17 @@
+export default {
+  name: 'home',
+  type: 'document',
+  title: 'Home',
+  fields: [
+    {
+      name: 'header',
+      type: 'string',
+      title: 'Header'
+    },
+    {
+      name: 'dualPic',
+      type: 'image',
+      title: 'Dual Picture'
+    }
+  ]
+}
