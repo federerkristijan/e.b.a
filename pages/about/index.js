a506d4afec265e47199bbd45f735fc89e26cf7d0
federerkristijan <federer.kristijan@gmail.com>
2023-01-30 00:12:56 +0100

    About.module.css

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,17 +1,19 @@
 import Image from "next/image"
 import Link from "next/link"
 
+import styles from "@/styles/About.module.css";
+
 const About = () => {
   return (
-    <div>
+    <div className={styles.about}>
     <div>
       <Link href="/katrix">
-        <Image src="/" alt="katrix"/>
+        <Image src="/" alt="katrix" className={styles.photo}/>
       </Link>
     </div>
     <div>
       <Link href="/vyra">
-        <Image src="/" alt=""/>
+        <Image src="/" alt="" className={styles.photo}/>
       </Link>
     </div>
     </div>
