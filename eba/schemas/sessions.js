3b871c0ecf02ff64e2fae6073f3d03022523370b
federerkristijan <federer.kristijan@gmail.com>
2023-01-29 18:19:33 +0100

    sessions.js, added images array of image

--- eba/schemas/sessions.js
+++ eba/schemas/sessions.js
@@ -4,14 +4,16 @@ export default {
   title: 'Sessions',
   fields: [
     {
-      name: 'header',
-      type: 'string',
-      title: 'Header'
-    },
-    {
-      name: 'image',
-      type: 'image',
-      title: 'Image'
+      name: 'images',
+      type: 'array',
+      title: 'Images',
+      of: [
+        {
+          name: 'image',
+          type: 'image',
+          title: 'Image'
+        }
+      ]
     }
   ]
 }
