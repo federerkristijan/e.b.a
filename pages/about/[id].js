7f2ec30bccc8611dedf352f721a36b3aab8f073d
federerkristijan <federer.kristijan@gmail.com>
2023-01-30 22:42:12 +0100

    not rendering sanity content

--- pages/about/[id].js
+++ pages/about/[id].js
@@ -1,15 +1,38 @@
+import Image from "next/image";
+import Link from "next/link";
 import sanityClient from "../../lib/client";
+import imageUrlBuilder from "@sanity/image-url";
 
 // const query = ``;
 
 const AboutMe = ({ about }) => {
+  const builder = imageUrlBuilder(sanityClient);
+
+  function urlFor(source) {
+    return builder.image(source);
+  }
+
   return (
     <div>
-      <p>hello honey</p>
+      <p>hello</p>
+      {about &&
+        about.map((item) => (
+          <div key={item.name}>
+            <h2>{item.name}</h2>
+            <h4>{item.bio}</h4>
+            <Image
+              src={urlFor(item.photo).width(498).url()}
+              alt={item.name}
+              className={styles.photo}
+              width={498}
+              height={512}
+            />
+            <Link href={item.url}><p>{item.url}</p></Link>
+          </div>
+        ))}
     </div>
-  )
-}
-
+  );
+};
 
 export async function getStaticPaths() {
   const about = await sanityClient.fetch(`*[_type == "about"]{
@@ -23,14 +46,10 @@ export async function getStaticPaths() {
   //   params: { id: about._id.toString() }
   // }))
 
-
   return {
     fallback: false,
-    paths: [
-      { params: { id: 'Katrix' }},
-      { params: { id: 'Lady Vyra' }}
-    ]
-  }
+    paths: [{ params: { id: "Katrix" } }, { params: { id: "Lady Vyra" } }],
+  };
 }
 
 export async function getStaticProps(context) {
@@ -51,9 +70,9 @@ export async function getStaticProps(context) {
         name: dominatrix.name || null,
         bio: dominatrix.bio || null,
         photo: dominatrix.photo || null,
-        url: dominatrix.url || null
-      }
-    }
+        url: dominatrix.url || null,
+      },
+    },
   };
 }
 
