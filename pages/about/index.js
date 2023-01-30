312c72d10a56e31cf717e33ae9c7a84236b0e774
federerkristijan <federer.kristijan@gmail.com>
2023-01-30 13:24:14 +0100

    about.js done - need a  bit more css

--- pages/about/index.js
+++ pages/about/index.js
@@ -2,18 +2,20 @@ import Image from "next/image"
 import Link from "next/link"
 
 import styles from "@/styles/About.module.css";
+import Dasa from "../../styles/assets/Dasa1.png";
+import Vyra from "../../styles/assets/Vyra1.png";
 
 const About = () => {
   return (
     <div className={styles.about}>
     <div>
       <Link href="/katrix">
-        <Image src="/" alt="katrix" className={styles.photo}/>
+        <Image src={Dasa} alt="katrix" className={styles.photo} width={498} height={512} />
       </Link>
     </div>
     <div>
       <Link href="/vyra">
-        <Image src="/" alt="" className={styles.photo}/>
+        <Image src={Vyra} alt="" className={styles.photo} width={498} height={512} />
       </Link>
     </div>
     </div>
--- pages/about/katrix.js
+++ pages/about/katrix.js
@@ -1,7 +1,21 @@
 const katrix = () => {
   return (
-    <div>katrix</div>
+    <div>
+
+    </div>
   )
+}
+
+export async function getStaticProps() {
+  const katrix = await sanityClient.fetch(`*[_type == "about"]`);
+
+  return {
+    props: {
+      katrix: {
+        
+      }
+    },
+  };
 }
 
 export default katrix
