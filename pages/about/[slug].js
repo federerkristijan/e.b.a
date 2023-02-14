25105d2cc2bc22aaa8691c303176a5c0fbbc6e7b
federerkristijan <federer.kristijan@gmail.com>
2023-02-14 14:49:05 +0100

    fetchDominatrix.js - < [slug].js still nto rendering

--- pages/about/[slug].js
+++ pages/about/[slug].js
@@ -2,54 +2,59 @@ import Image from "next/image";
 import Link from "next/link";
 import { sanityClient, urlFor } from "@/lib/sanity";
 // import { getClient } from "@/lib/sanity.server";
-import { dominatrixQuery, slugPathQuery } from "@/lib/queries";
+import { dominatrixQuery, slugPathQuery, dominatrixBySlugQuery } from "@/lib/queries";
 import { useRouter } from "next/router";
 import ErrorPage from "next/error";
 
+export async function generateStaticParams() {
+  return [{ id: '1' }, { id: '2' }]
+}
+
 async function getDominatrix() {
-  const res = await fetch('slugPathQuery', {
+  const res = await sanityClient.fetch(`*[_type == "dominatrix" && defined(slug.current)][1].slug.current`, {
     next: { revalidate: 10 },
   });
   const data = await res.json();
   return data;
 }
 
-
 const AboutMe = async ({ params }) => {
   const dominatrix = await getDominatrix(params.id);
-  const router = useRouter();
+
+  const domina = dominatrix?.map((domina) => (
+    <div key={domina.slug}>
+    <h2>{domina.name}</h2>
+    <h4>{domina.bio}</h4>
+    <Image
+      src={urlFor(domina.photo).url()}
+      alt={domina.name}
+      width={498}
+      height={512}
+    />
+    <h1>test</h1>
+    <Link href={domina.url}>
+      <p>{domina.url}</p>
+    </Link>
+  </div>))
+
+  // const router = useRouter();
 
   // console.log(dominatrix)
 
-  const slug = data?.dominatrix?.slug;
+  // const slug = data?.dominatrix?.slug;
 
-  if (!router.isFallback && !slug) {
-    return <ErrorPage statusCode={404} />;
-  }
+  // if (!router.isFallback && !slug) {
+  //   return <ErrorPage statusCode={404} />;
+  // }
 
   //  need: rendering just one object of the array, not the both
   return (
-    <main preview={preview}>
-      {router.isFallback ? (
-        <div>Loading...</div>
-      ) : (
-        <div key={dominatrix._id}>
-          <h2>{dominatrix.name}</h2>
-          <h4>{dominatrix.bio}</h4>
-          <Image
-            src={urlFor(dominatrix.photo).url()}
-            alt={dominatrix.name}
-            width={498}
-            height={512}
-          />
-          <h1>test</h1>
-          <Link href={dominatrix.url}>
-            <p>{dominatrix.url}</p>
-          </Link>
-        </div>
-      )}
-    </main>
-  );
+    <div>
+      {domina}
+    </div>
+  )
+
+
 };
 
 export default AboutMe;
